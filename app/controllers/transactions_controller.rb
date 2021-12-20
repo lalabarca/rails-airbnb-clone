1e29e1316b92b9d5f05f41e60bb80de7653228e5
Laura Hardy-Dessources <laura.hardydes@gmail.com>
2021-12-20 11:52:07 -0400

    controller transaction terminé

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,15 +1,21 @@
 class TransactionsController < ApplicationController
-  def new
-    @product = Product.find(params[:id])
-    @transaction = Transaction.new
-    @user = current_user
-  end
+  # def new
+  #   @product = Product.find(params[:id])
+  #   @transaction = Transaction.new
+  #   @user = current_user
+  # end
 
   def create
-    @product = Product.find(params[:id])
+    @product = Product.find(params[:product_id])
     @user = current_user
+    @transaction = Transaction.new
     @transaction.product = @product
     @transaction.user = @user
     @transaction.price = @product.price * @transaction.quantity
+    if @transaction.save!
+      redirect_to "products/index"
+    else
+      redirect_to "products/show"
+    end
   end
 end
