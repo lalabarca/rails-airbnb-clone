a9b08a94f84ff18a87a40f129849fee6a5e3e31b
Laura Hardy-Dessources <laura.hardydes@gmail.com>
2021-12-21 11:03:58 -0400

    transaction index Panier terminé

---
+++ app/javascript/packs/delete-button.js
@@ -0,0 +1,8 @@
+const deleteBtn = () => {
+  const button = document.querySelector(".btn-delete");
+  const article = document.querySelector(".my-product");
+  button.addEventListener("click", (event) => {
+    event.preventDefault();
+
+  })
+};
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   end
 
   resources :shops, only: [:show]
+  resources :transactions, only: [:index, :destroy]
 end
